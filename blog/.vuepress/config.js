d73fee96e9bd865216e075746a8f5fd4da296685
Zemian Deng <zemian.deng@oracle.com>
2020-08-07 00:35:54 -0400

    Setup vuepress blog site generator

---
+++ blog/.vuepress/config.js
@@ -0,0 +1,18 @@
+module.exports = {
+	title: "Zemian's Blog",
+	description: "Musing on programming stuff",
+	theme: '@vuepress/theme-blog',
+	themeConfig: {
+		footer: {
+	      contact: [
+	        {
+	          type: 'github',
+	          link: 'https://github.com/zemian',
+	        }
+	      ]
+	    },
+	  },
+	  markdown: {
+	    linkify: true
+	  }
+}
---
+++ readme.md
@@ -0,0 +1,6 @@
+This is Zemian Deng Personal Web Blog
+
+## To write a new blog
+
+1. Create a new file under `blog/_posts/<year-month-date-topic>.md`
+2. Run `npm run dev` to preview it on <http://localhost:8080>
