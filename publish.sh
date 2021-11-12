7e36a341843fabd8f3c93e99f86502c5ad79ea14
Zemian Deng <zemiandeng@gmail.com>
2021-11-12 06:42:13 -0500

    Fix publish.sh to use a temp repo

--- publish.sh
+++ publish.sh
@@ -3,15 +3,17 @@
 ./vendor/bin/sculpin generate --env=prod
 if [ $? -ne 0 ]; then echo "Could not generate the site"; exit 1; fi
 
-git checkout --orphan gh-pages
-mkdir -p temp/working_dir
-mv * temp/working_dir
-mv temp/working_dir/output_prod/* .
+REPO=temp/zemian.github.io.git_gh-pages
+if [[ ! -d $REPO ]]; then
+    git clone -b gh-pages git@github.com:zemian/zemian.github.io.git $REPO
+fi
+
+pushd $REPO
+rm -rf *
+cp -rf ../../output_prod/* .
 git add .
 git commit -m 'Publish'
 git push -f origin gh-pages
-git checkout master
-git branch -D gh-pages
-cp -rf temp/working_dir/* .
+popd
 
 echo "Publish is complete"
